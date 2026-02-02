6c11c764af2b5b7ea54e6fa36c789c7f8a94d07b
Shen Cheng <cheng423@acn137.agate.msi.umn.edu>
2026-02-02 14:39:49 -0600

    wk6 slides

---
+++ model1/models/bs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --time=12:00:00
+#SBATCH --mem=10g
+#SBATCH --tmp=10g
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH -L nonmem@slurmdb:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cheng423@umn.edu
+#SBATCH -p msilarge
+#SBATCH --output=NONMEM-BS.log        # Log file name
+
+ulimit -l unlimited
+
+module load nonmem/750-rocky8
+
+bootstrap mod1.ctl -samples=500 -seed=12135 -rplots=2
